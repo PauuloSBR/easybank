d4d30a2e0e7b3d68ba596e28984d183b7282e071
PauuloSBR <pauloamador00@hotmail.com>
2023-04-28 18:35:24 -0300

    Finish menu mobile

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 var hamburguerButton = document.querySelector("#hamburguer-button");
+var iconBurguer = hamburguerButton.querySelector("i");
 var links = document.querySelector(".myLinks");
 hamburguerButton === null || hamburguerButton === void 0 ? void 0 : hamburguerButton.addEventListener("click", function () {
     links === null || links === void 0 ? void 0 : links.classList.toggle("show");
+    iconBurguer === null || iconBurguer === void 0 ? void 0 : iconBurguer.classList.toggle("burguer-close");
 });
--- script/script.ts
+++ script/script.ts
@@ -1,6 +1,8 @@
 
 const hamburguerButton = document.querySelector("#hamburguer-button") as HTMLButtonElement;
+const iconBurguer = hamburguerButton.querySelector("i");
 const links = document.querySelector(".myLinks");
 hamburguerButton?.addEventListener("click",()=>{
     links?.classList.toggle("show");
+    iconBurguer?.classList.toggle("burguer-close");
 })
