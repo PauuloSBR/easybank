17660037ea1296b954fda82e4f4fccaa6e6be0e9
PauuloSBR <pauloamador00@hotmail.com>
2023-04-29 08:51:49 -0300

    Fix the mobile menu , now works well . Start the main part of web site.

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 var hamburguerButton = document.querySelector("#hamburguer-button");
 var iconBurguer = hamburguerButton.querySelector("i");
 var links = document.querySelector(".myLinks");
+var header = document.querySelector("#header");
 hamburguerButton === null || hamburguerButton === void 0 ? void 0 : hamburguerButton.addEventListener("click", function () {
     links === null || links === void 0 ? void 0 : links.classList.toggle("show");
     iconBurguer === null || iconBurguer === void 0 ? void 0 : iconBurguer.classList.toggle("burguer-close");
+    header === null || header === void 0 ? void 0 : header.classList.toggle("header-active");
 });
--- script/script.ts
+++ script/script.ts
@@ -2,7 +2,9 @@
 const hamburguerButton = document.querySelector("#hamburguer-button") as HTMLButtonElement;
 const iconBurguer = hamburguerButton.querySelector("i");
 const links = document.querySelector(".myLinks");
+const header = document.querySelector("#header");
 hamburguerButton?.addEventListener("click",()=>{
     links?.classList.toggle("show");
     iconBurguer?.classList.toggle("burguer-close");
+    header?.classList.toggle("header-active");
 })
